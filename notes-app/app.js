c9e385d2b8db2ff300de39dd468e5cb83e4daa8e
krishan-kant <krish.kant.47@gmail.com>
2020-04-24 15:50:16 +0530

    parsing arguments with yarg

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,96 @@
-const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title : ',argv.title)
+        console.log('Body : ',argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse() //required for yargs else it was not printing anything
+
+// const validator = require('validator')
+// const y = require('yargs')
+// const chalk = require('chalk')
+
+// y.version('1.1.0')
+// console.log(y.argv)
+
+// y.command({
+//     command: 'add',
+//     describe: 'adding a note',
+//     handler: function(){
+//         console.log('adding the note')
+//     }
+// })
+
+//const command = process.argv[2]
+
+// if (command === 'add'){
+//     log('Adding')
+// }else if(command === 'remove'){
+//     log('Removing')
+// }else{
+//     log(chalk.red.inverse('Command not found'))
+// }
 
 
-const log= console.log
+// const getNotes=require('./notes.js')
 
-log(chalk.green.inverse('Hara rang hai ye'))
+// const message=getNotes()
 
-const getNotes=require('./notes.js')
+// console.log(message)
 
-const message=getNotes()
+// //console.log(validator.isEmail('krish.kant.47@gmail.com'))
+// console.log(validator.isURL('www.google.com'))
 
-console.log(message)
+// log(chalk.blue.inverse('Success!'))
 
-//console.log(validator.isEmail('krish.kant.47@gmail.com'))
-console.log(validator.isURL('www.google.com'))
+// log(process.argv[2])
