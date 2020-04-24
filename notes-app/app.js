2989dc940763d06d53de950372de5034fb8ddb50
krishan-kant <krish.kant.47@gmail.com>
2020-04-24 12:21:49 +0530

    validator added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,10 @@
+const validator = require('validator')
+
 const getNotes=require('./notes.js')
 
 const message=getNotes()
 
 console.log(message)
+
+//console.log(validator.isEmail('krish.kant.47@gmail.com'))
+console.log(validator.isURL('www.google.com'))
