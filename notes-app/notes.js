21d3dbb1b051bfa3e165bd92ea3995d7e002abb7
krishan-kant <krish.kant.47@gmail.com>
2020-04-24 16:29:49 +0530

    adding notes and storing in json file

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,45 @@
+const fs= require('fs')
+
 const getNotes = function(){
     const message='Your Notes...'
     return message
 }
 
-module.exports = getNotes
+const addNote = function(title,body){
+    const notes=loadNotes()
+    const duplicatenote= notes.filter(function(note){
+        return note.title===title
+    })
+
+    if(duplicatenote.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note taken')
+    }
+    else{
+        console.log("Note already taken!")
+    }
+}
+
+const saveNotes = function(notes){
+    const dataJSON=JSON.stringify(notes)
+    fs.writeFileSync('notes.json',dataJSON)
+}
+
+const loadNotes = function(){
+    try{
+        const databuffer = fs.readFileSync('notes.json')
+        const dataJSON= databuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch(e){
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
