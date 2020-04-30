c5150305400fec1e6061572233f5df8d431d7142
krishan-kant <krish.kant.47@gmail.com>
2020-04-30 12:05:22 +0530

    Errors detection and manual city selectin added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,22 @@
 // key 89d934ceac10b44ff02ba21b28ee3901
-
 //http://api.weatherstack.com/current?access_key=89d934ceac10b44ff02ba21b28ee3901&query=kolkata
 
 const request = require('request')
+const city=process.argv[2]
+const url="http://api.weatherstack.com/current?access_key=89d934ceac10b44ff02ba21b28ee3901&query="+encodeURIComponent(city)
 
-const url="http://api.weatherstack.com/current?access_key=89d934ceac10b44ff02ba21b28ee3901&query=kolkata"
-
-request(url, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request((url), (error,response) => {
+    if(error){
+        console.log("No result found, check your connectivity and enter valid location")
+    }
+    else{
+        const data = JSON.parse(response.body)  
+        if(data.error){
+            console.log("Location not found")
+        }
+        else{
+            console.log("City:",data.location.name,"\nRegion:",data.location.region,"\nCountry:",data.location.country)
+            console.log(data.current.weather_descriptions[0],"\nThe current temperature is",data.current.temperature,"C\nHumidity is",data.current.humidity)
+        }
+    }
 })
-
-// console.log("Starting...")
-
-// setTimeout(() => {
-//     console.log("2 second timeout")
-// },2000)
-
-// setTimeout(() => {
-//     console.log("0 second Timeout")
-// },0)
-
-
-// console.log("...Stopping")
