ed1bf0222207ad2b37d58154c6b4c6dc92ffa3a6
krishan-kant <krish.kant.47@gmail.com>
2020-05-02 11:54:08 +0530

    Files updated

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log("Script running")
+
+
+const weather_form = document.querySelector('form')
+const search = document.querySelector('input')
+const message_1 = document.querySelector('#message_1')
+const message_2 = document.querySelector('#message_2')
+
+
+weather_form.addEventListener('submit',(e) => {
+    
+    e.preventDefault()
+    
+    const location=search.value
+
+    message_1.textContent='Loading...'
+    message_2.textContent=''
+
+    console.log(location)
+
+    const url="http://localhost:3000/weather?search="+encodeURIComponent(location)
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                message_1.textContent = "Location not found"
+            }
+            else{
+                const location_output="City:"+data.location.name+"\nRegion:"+data.location.region+"\nCountry:"+data.location.country
+                message_1.textContent=location_output
+                const weather_output=data.current.weather_descriptions[0]+" The current temperature is "+data.current.temperature+"C, Humidity is "+data.current.humidity
+                message_2.textContent=weather_output
+                
+            }
+        })
+    })
+    console.log('Testing')
+})
---
+++ src/app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const path= require('path')
+const hbs= require('hbs')
+
+const log= console.log
+
+
+//log(__dirname)
+const public_path=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+//log(__filename)
+
+const app=express()
+
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+app.use(express.static(public_path))
+
+app.get('',(req,res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Krishan Kant'
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        title: 'Help',
+        name: 'Krishan Kant'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'About me',
+        name: 'Krishan Kant'
+    })
+})
+
+const request = require('request')
+
+app.get('/weather',(req,res) => {
+    if(!req.query.search){
+        return res.send({
+            error: "You need to provide an address!"
+        })
+    }
+    const city=req.query.search
+    const url="http://api.weatherstack.com/current?access_key=89d934ceac10b44ff02ba21b28ee3901&query="+encodeURIComponent(city)
+    request((url), (error,response) => {
+        if(error){
+            res.send("No result found, check your connectivity and enter valid location")
+        }
+        else{
+            const data = JSON.parse(response.body)  
+                return res.send(data)
+        }
+    })
+})
+
+app.get('/help/*',(req,res) => {
+    res.render('notfound',{
+        title: 'Error 404',
+        message: 'Help page not found',
+        name: 'Krishan Kant'
+    })
+})
+
+app.get('*',(req,res) => {
+    res.render('notfound',{
+        title: 'Error 404',
+        message: 'Page not found',
+        name: 'Krishan Kant'
+    })
+})
+
+app.listen(3000,() => {
+    console.log('Server is up on port 3000')
+})
