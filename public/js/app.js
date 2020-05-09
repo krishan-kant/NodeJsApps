5be5cab4748724b09e663ca9653016e2978fbcd0
krishan-kant <krish.kant.47@gmail.com>
2020-05-09 23:29:47 +0530

    mobile friendly

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const weather_form = document.querySelector('form')
 const search = document.querySelector('input')
 const message_1 = document.querySelector('#message_1')
 const message_2 = document.querySelector('#message_2')
-
+const message_3 = document.querySelector('#message_3')
 
 weather_form.addEventListener('submit',(e) => {
     
@@ -15,6 +15,7 @@ weather_form.addEventListener('submit',(e) => {
 
     message_1.textContent='Loading...'
     message_2.textContent=''
+    message_3.textContent=''
 
     console.log(location)
 
@@ -25,11 +26,12 @@ weather_form.addEventListener('submit',(e) => {
                 message_1.textContent = "Location not found"
             }
             else{
-                const location_output="City:"+data.location.name+"\nRegion:"+data.location.region+"\nCountry:"+data.location.country
+                const location_output="City: "+data.location.name+", Region: "+data.location.region+", Country: "+data.location.country
                 message_1.textContent=location_output
-                const weather_output=data.current.weather_descriptions[0]+" The current temperature is "+data.current.temperature+"C, Humidity is "+data.current.humidity
+                const weather_output=data.current.weather_descriptions[0]+", Current Temp: "+data.current.temperature+"°C, Humidity: "+data.current.humidity
                 message_2.textContent=weather_output
-                
+                const weather_2="Feels like: "+data.current.feelslike+"°C, Cloud cover: "+data.current.cloudcover+", Wind Speed: "+data.current.wind_speed            
+                message_3.textContent=weather_2
             }
         })
     })
